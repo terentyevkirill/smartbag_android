c6fe60031f8f16de0a0e8e2c355d65099062e023
carinaHSA <carina.strehler@hs-augsburg.de>
2020-02-13 16:09:36 +0200

    Adding demo Button and some TextViews

--- app/src/main/java/com/pnit/smartbag/ui/home/HomeFragment.java
+++ app/src/main/java/com/pnit/smartbag/ui/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,20 +16,47 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.pnit.smartbag.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_steps);
+
+        final TextView text_weekday = root.findViewById(R.id.text_weekday);
+        final TextView text_date = root.findViewById(R.id.text_date);
+
+        Calendar calendar = Calendar.getInstance();
+        Date date = calendar.getTime();
+        text_weekday.setText(new SimpleDateFormat("EEEE", Locale.ENGLISH).format(date.getTime()));
+        text_date.setText(new SimpleDateFormat("MMMM, dd", Locale.ENGLISH).format(date.getTime()));
+
+        final ProgressBar pb_goal = root.findViewById(R.id.pb_goal);
+        pb_goal.setMax(homeViewModel.getGoal());
+
+        final TextView text_steps = root.findViewById(R.id.text_steps);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                text_steps.setText(s);
+                pb_goal.setProgress(pb_goal.getProgress() + 1);
+            }
+        });
+
+        final Button btn_demo = root.findViewById(R.id.btn_demo);
+        btn_demo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                homeViewModel.newStep();
             }
         });
         return root;
--- app/src/main/java/com/pnit/smartbag/ui/home/HomeViewModel.java
+++ app/src/main/java/com/pnit/smartbag/ui/home/HomeViewModel.java
@@ -7,13 +7,22 @@ import androidx.lifecycle.ViewModel;
 public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    private final int GOAL_DAILY_STEPS = 100; //just for demonstrating purposes
+    private int currentSteps = 0;
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("#Steps");
+        mText.setValue(String.valueOf(currentSteps));
     }
 
     public LiveData<String> getText() {
         return mText;
+    }
+
+    public int getGoal(){return GOAL_DAILY_STEPS;}
+
+    public void newStep(){
+        currentSteps++;
+        mText.setValue(String.valueOf(currentSteps));
     }
 }
