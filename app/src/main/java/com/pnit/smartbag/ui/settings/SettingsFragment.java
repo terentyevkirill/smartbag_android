6cf599765b05ed0c0697a373178a5e624c595359
danzakusilo <60900104+EchoAstral@users.noreply.github.com>
2020-02-17 19:17:06 +0200

    Layout for settigns fragment

--- app/src/main/java/com/pnit/smartbag/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/pnit/smartbag/ui/settings/SettingsFragment.java
@@ -4,32 +4,48 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.pnit.smartbag.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class SettingsFragment extends Fragment {
 
+    @BindView(R.id.test_switch)
+    Switch testSwitch;
+    @BindView(R.id.testTextView)
+    TextView test_tv;
+    @BindView(R.id.logoutLayout)
+    ConstraintLayout logoutLayout;
+    @BindView(R.id.logoutString)
+    TextView logoutString;
+
     private SettingsViewModel settingsViewModel;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         settingsViewModel =
                 ViewModelProviders.of(this).get(SettingsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        settingsViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        ButterKnife.bind(this, root);
+
+        testSwitch.setChecked(settingsViewModel.getSwitchState());
+        test_tv.setText(settingsViewModel.getTestString());
+        logoutLayout.setOnClickListener(v -> logoutString.setText("Test if clickable layout works"));
+        logoutString.setText(settingsViewModel.getLogout());
         return root;
     }
+
 }
--- app/src/main/java/com/pnit/smartbag/ui/settings/SettingsViewModel.java
+++ app/src/main/java/com/pnit/smartbag/ui/settings/SettingsViewModel.java
@@ -6,14 +6,23 @@ import androidx.lifecycle.ViewModel;
 
 public class SettingsViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    public static final String TEST_STRING = "This is a test switch";
+    public static final String LOGOUT = "Log out";
+    private boolean switchState = false;
 
-    public SettingsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is settings fragment");
+
+    public boolean getSwitchState(){
+        return switchState;
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public String getTestString(){
+        return TEST_STRING;
     }
+
+    public String getLogout(){return LOGOUT;}
+
+    public void logout(){
+        // TODO a proper loguot method
+    }
+
 }
