0b19f07b8d54ae0ee151a6c6c556733897d04a0d
Paula-lis <plischewski@hotmail.de>
2020-02-18 11:11:47 +0100

    First version of profile fragment

--- app/src/main/java/com/pnit/smartbag/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/pnit/smartbag/ui/profile/ProfileViewModel.java
@@ -1,19 +1,26 @@
 package com.pnit.smartbag.ui.profile;
 
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.pnit.smartbag.R;
+
 public class ProfileViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
-
     public ProfileViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is profile fragment");
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public String BMIcalc (Float hight, Float weight){
+        Float bmicalc = (weight/(hight*hight))*10000;
+        return bmicalc.toString();
     }
+
+    public String getBMIcalc(Float hight, Float weight){return BMIcalc(hight, weight); }
+
+    /*public LiveData<String> getText() {
+        return mText;
+    }*/
 }
