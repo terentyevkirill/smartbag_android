e160a8ec8f681ac6684fe1e860444cb4a853d677
terentyevkirill <kirillterentyev1999@gmail.com>
2020-02-25 14:52:59 +0200

    Changes from rating branch added

--- app/src/main/java/com/pnit/smartbag/ui/rating/RatingFragment.java
+++ app/src/main/java/com/pnit/smartbag/ui/rating/RatingFragment.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,9 +17,18 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.pnit.smartbag.R;
+import com.pnit.smartbag.data.user.model.User;
 import com.pnit.smartbag.ui.settings.SettingsViewModel;
 
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class RatingFragment extends Fragment {
+
+    @BindView(R.id.rating_rv)
+    RecyclerView recyclerView;
 
     private RatingViewModel ratingViewModel;
 
@@ -27,12 +38,13 @@ public class RatingFragment extends Fragment {
                 ViewModelProviders.of(this).get(RatingViewModel.class);
         View root = inflater.inflate(R.layout.fragment_rating, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
-        ratingViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        ButterKnife.bind(this, root);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        RatingUserAdapter adapter = new RatingUserAdapter(new ArrayList<User>(), getContext());
+        recyclerView.setAdapter(adapter);
+
         return root;
     }
 
---
+++ app/src/main/java/com/pnit/smartbag/ui/rating/RatingUserAdapter.java
@@ -0,0 +1,62 @@
+package com.pnit.smartbag.ui.rating;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.pnit.smartbag.R;
+import com.pnit.smartbag.data.user.model.User;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RatingUserAdapter extends RecyclerView.Adapter<RatingUserAdapter.UserViewHolder> {
+    private ArrayList<User> ratingList;
+
+    public RatingUserAdapter(ArrayList<User> ratingList, Context context) {
+        this.ratingList = ratingList;
+    }
+
+    @Override
+    public int getItemCount() {
+        return ratingList.size();
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.rating_card, viewGroup, false);
+        v.getBackground().setAlpha(0);
+        ButterKnife.bind(this, v);
+        UserViewHolder holder = new UserViewHolder(v);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder userViewHolder, int i) {
+        userViewHolder.position.setText("1");
+        userViewHolder.userName.setText(ratingList.get(i).getUsername());
+        userViewHolder.steps.setText("20000");
+    }
+
+    public class UserViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.position_card_tv)
+        TextView position;
+        @BindView(R.id.username_card_tv)
+        TextView userName;
+        @BindView(R.id.steps_card_tv)
+        TextView steps;
+
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+            // TODO: retrieve data for view elements
+        }
+    }
+}
